8ff7cb799e781c4246209557f1b734e0c7cde6c7
demis <swordno@163.com>
2023-05-18 14:55:39 +0800

    initial

---
+++ strings.rus.h
@@ -0,0 +1,63 @@
+﻿#define S_MENU_RUS0 L"&Файл"
+#define S_MENU_RUS1 L"&Выполнить"
+#define S_MENU_RUS2 L"&Настройки"
+#define S_MENU_RUS3 L"&Помощь"
+
+#define RUS_0 L"&О программе...\ta"
+#define RUS_1 L"&Клавиатурные Сокращения"
+#define RUS_2 L"Выбрать &Цель...\tt"
+#define RUS_3 L"&Контроль...\tc"
+#define RUS_4 L"&Снять все лимиты\tu"
+#define RUS_5 L"&Снять все лимиты"
+#define RUS_6 L"&Режим реального времени"
+#define RUS_7 L"&Писать лог"
+#define RUS_8 L"&Следить...\tw"
+#define RUS_9 L"&Перестать следить"
+#define RUS_10 L"&Посетить домашнюю страницу"
+
+
+#define RUS_16 L"&Китайский (упрощенный)"
+#define RUS_17 L"Китайский (&традиционный)"
+#define RUS_18 L"&Английский (English)"
+#define RUS_19 L"&Финский"
+#define RUS_20 L"Фра&нцузский"
+#define RUS_21 L"&Японский"
+#define RUS_22 L"&Испанский"
+#define RUS_23 L"Я&понский (Ямато)"
+#define RUS_24 L"&Восстановить окно BES"
+#define RUS_25 L"В&ыход\tx"
+#define RUS_26 L"В&ыход"
+#define RUS_27 L"Выйти &принудительно\tCtrl+F9"
+#define RUS_28 L"С&охранить список процессов..."
+#define RUS_29 L"&Язык (Language)"
+
+// #define ENG_31 TEXT( "R&ussian" )
+#define RUS_31 L"&Русский"
+#define FRE_31 L"R&usse"
+#define FIN_31 L"&venäjä"
+#define JPN_31 L"ロシア語 (&U)"
+#define SPA_31 L"R&uso"
+#define CHT_31 L"俄語 (&U)"
+#define CHS_31 L"俄语 (&U)"
+
+#define S_FORMAT1_RUS "[ %s ]\r\nИмя файла: %s\r\nЗаголовок окна: %s\r\nID процесса: 0x%08X (%lu)\r\n\
+включая %u поток(и)"
+
+#define S_QUESTION1_RUS L"BES ограничит использование ЦП этим процессом.\r\n\r\nПродолжить?"
+#define S_QUESTION2_RUS L"BES ограничит использование ЦП этим процессом и продолжит следить за ним даже после его закрытия.\
+\r\n\r\nПродолжить?"
+
+#define S_FORMAT2_RUS L"EXE файл: %s\r\n\r\nID процесса: 0x%08X (%lu)\r\n\r\nЗаголовок окна: %s"
+
+#define S_TOOLTIP_RUS0 L"Выбрать целевой процесс"
+#define S_TOOLTIP_RUS1 L"Остановить ограничение и/или слежение полностью"
+#define S_TOOLTIP_RUS2 L"Управление степенью ограничения процесса"
+#define S_TOOLTIP_RUS3 L"Завершить программу"
+
+#define S_RUS_0001 L"Русский язык"
+#define S_RUS_1000 L"Какой процесс вы хотите ограничить?"
+#define S_RUS_1001 L"Подтверждение"
+#define S_RUS_2000 L"Ограничение нагрузки ЦП:"
+#define S_RUS_9000 L"О программе BES"
+#define S_RUS_DESC L"\r\nКонтроллирует загрузку ЦП для каждого процесса."
+
